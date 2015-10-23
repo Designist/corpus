5c2844e048d3d40ce02aec01e9c02f5c9bbddbe4
Nicholas Tomlin <nicholas_tomlin@brown.edu>
2015-10-23 13:54:23 -0400

    Added documentation

--- splitTokenize.hs
+++ splitTokenize.hs
@@ -1,1 +1,5 @@
+-- splitTokenize : String -> [[String]]
+-- I/P : text, a string of text
+-- O/P : a list of lists, where each sublist represents a new line,
+--       and the contents of the list are the words in the line
 splitTokenize text = map words (lines text)
