1ff8e6f01739b90cc9c6360cc9a391099fed2980
Nicholas Tomlin <nicholas_tomlin@brown.edu>
2015-10-23 18:05:33 -0400

    Updated type signature

--- averageLength.hs
+++ averageLength.hs
@@ -1,4 +1,4 @@
--- averageLength : (Fractional b) => [[a]] -> b
+-- averageLength :: (Fractional a, Foldable t) => [t a1] -> a
 -- I/P : ls, a list of strings
 -- O/P : the average length of strings in ls
 averageLength alos  = fromIntegral (sum (map length alos)) /
