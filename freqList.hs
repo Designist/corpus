efb5f240abeb0c5497866722e348609951bde547
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-10-25 16:25:11 -0400

    merged `countElem` with freqList
    the countElem procedure is now included in the FreqList module

--- freqList.hs
+++ freqList.hs
@@ -2,7 +2,14 @@ module FreqList where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
-import CountElem
+
+-- countElem :: (Ord k) => Data.Map.Map k Int -> k -> Data.Map.Map k Int
+-- I/P : base, a base map to be passed; usually Data.Map.empty
+--       alow, a list of words
+-- O/P : the base value with the frequency of alow
+countElem base alow = case (Map.lookup alow base) of
+                       Just v -> Map.insert alow (v + 1) base
+                       Nothing -> Map.insert alow 1 base
 
 -- freqList :: (Ord k) => [k] -> Data.Map.Map k Int
 -- I/P : alow, a list of words
