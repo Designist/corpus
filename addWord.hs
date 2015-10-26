c6038d421ac2ecfb61b07f7a5d3649a522be4f40
Nicholas Tomlin <nicholas_tomlin@brown.edu>
2015-10-26 02:50:33 -0400

    fixed paren style

--- addWord.hs
+++ addWord.hs
@@ -4,5 +4,5 @@ module AddWord where
 -- I/P : alow, a list of words
 --       word, a word
 -- O/P : a new list containing word and the elements of alow
-addWord alow word = if (elem word alow) then alow
+addWord alow word = if elem word alow then alow
                     else word : alow
