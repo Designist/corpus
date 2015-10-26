f437f112ab181fde7ad4fd5248b9fcad2610bcf6
Nicholas Tomlin <nicholas_tomlin@brown.edu>
2015-10-26 04:31:04 -0400

    Update freqBrown.hs
    module documentation, import ordering

--- freqBrown.hs
+++ freqBrown.hs
@@ -1,9 +1,12 @@
+-- | Finds the number of tiems a word appears in the Brown corpora.
+
 module FreqBrown where
+
+import FreqList
 
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified System.IO as IO
-import FreqList
 
 -- freqBrown :: Num a => String -> IO (Maybe a)
 -- I/P : word, a word
