2c827a0ca07b8c29f9efa747ebe6d46c2919b7f6
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-10-26 05:24:39 -0400

    Added skipBigrams

---
+++ skipBigrams.hs
@@ -0,0 +1,7 @@
+module SkipBigrams where
+
+-- I/P : alow, a list of words
+-- O/P : the set of sentence-order tuples of alow
+skipBigrams :: [a] -> [(a,a)]
+skipBigrams [] = []
+skipBigrams (x:alow) = [(x,y) | y <- alow] ++ skipBigrams alow
