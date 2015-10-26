bb483d7dcf682c2a0fb966dd69b945c2a8cae81d
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-10-26 05:26:12 -0400

    Updated bigram.hs
    Removed extraneous type signature, bigram case

--- bigram.hs
+++ bigram.hs
@@ -2,10 +2,8 @@
 
 module Bigram where
 
--- bigram :: [a] -> [[a]]
 -- I/P : alow, a list of words
 -- O/P : the bigrams of alow
 bigram :: [a] -> [[a]]
 bigram [] = []
-bigram [_] = []
 bigram alow = take 2 alow : bigram (tail alow)
